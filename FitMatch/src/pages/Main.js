9ab3f7d72144e48a9bc49830dd5bdc4d47636766
Bernardo <bernardo.chuecos@utexas.edu>
2018-04-17 02:59:36 -0500

    AddedButtons

--- FitMatch/src/pages/Main.js
+++ FitMatch/src/pages/Main.js
@@ -5,7 +5,8 @@ import {
 	Text,
 	View,
 	FlatList,
-	Image
+	Image,
+	TouchableOpacity
 } from 'react-native';
 
 export default class Main extends Component {
@@ -18,17 +19,33 @@ export default class Main extends Component {
 
 	renderItem = ({ item }) => {
 		return (
-			<View style={{ flex: 1, flexDirection: 'row', marginBottom: 3}}>
-				<Image style={{ width: 80, height: 80, margin: 5 }}
-					source={{ uri: item.image }} />
-				<View style={{ flex: 1, justifyContent: 'center', marginLeft: 5 }}>
-					<Text style={{ fontSize: 18, color: 'black', marginBottom: 15}}>
+			<View style={{ flexDirection: 'column',  marginTop: 100, marginBottom: 100}}>
+				<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+					<Image style={styles.image}
+						source={{ uri: item.image }} />
+				</View>
+				<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+					<Text style={styles.text}>
 						{item.book_title}
 					</Text>
 					<Text style={{ fontSize: 16, color: 'black'}}>
 						{item.author}
 					</Text>
 				</View>
+				<View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+       				<TouchableOpacity
+         				style={styles.button}>
+         				<Text> Yes </Text>
+       				</TouchableOpacity>
+       				<TouchableOpacity
+         				style={styles.button}>
+         				<Text> Maybe </Text>
+       				</TouchableOpacity>
+       				<TouchableOpacity
+         				style={styles.button}>
+         				<Text> No </Text>
+       				</TouchableOpacity>
+      			</View>
 			</View>
 		)
 	}
@@ -74,5 +91,20 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor:'#A1D3F8'
+	},
+	image: {
+		 width: 280, 
+		 height: 280
+	},
+	text: { fontSize: 18, 
+		color: 'black',
+		marginTop: 40, 
+		marginBottom: 15
+	},
+	button: {backgroundColor: '#DDDDDD', 
+	padding: 10,
+	marginTop: 50,
+	marginLeft: 20,
+	marginRight: 20
 	}
 })
